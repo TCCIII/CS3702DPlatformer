50056c60732b1307f95d7f3cb60ce1c103fc018f
TCCIII <chiffya@sunypoly.edu>
2020-04-12 23:39:51 -0400

    Added Rare Mushroom and Mythic Speed Potion
    Adjusted the height on a couple sprites. Added the Blue RareBackground and the Red MythicBackground. Mushroom is +2 Health and -1 Speed. Speed is + 4 Speed and -1 Jump

---
+++ MyFirstGame/Assets/Scripts/Gameplay/Items/MythicSpeedPotionInstance.cs
@@ -0,0 +1,48 @@
+﻿using Platformer.Gameplay;
+using UnityEngine;
+using Platformer.Model;
+using static Platformer.Core.Simulation;
+
+
+namespace Platformer.Mechanics
+{
+    [RequireComponent(typeof(Collider2D))]
+    public class MythicSpeedPotionInstance : MonoBehaviour
+    {
+        readonly PlatformerModel model = Core.Simulation.GetModel<PlatformerModel>();
+        public AudioClip speedPotionCollectAudio;
+        [Tooltip("If true, animation will start at a random position in the sequence.")]
+        public bool randomAnimationStartTime = false;
+        [Tooltip("List of frames that make up the animation.")]
+        public Sprite[] idleAnimation, collectedAnimation;
+
+        internal Sprite[] sprites = new Sprite[0];
+
+        internal SpriteRenderer _renderer;
+
+        internal int speedPotionIndex = -1;
+        internal SpeedPotionController controller;
+        internal int frame = 0;
+        internal bool collected = false;
+
+        void Awake()
+        {
+            _renderer = GetComponent<SpriteRenderer>();
+            if (randomAnimationStartTime)
+                frame = Random.Range(0, sprites.Length);
+            sprites = idleAnimation;
+        }
+
+        public void AddAbility()
+        {
+            model.player.maxSpeed = model.player.maxSpeed + 4;
+            model.player.jumpTakeOffSpeed = model.player.jumpTakeOffSpeed - 1;
+        }
+
+        public void RemoveAbility()
+        {
+            model.player.maxSpeed = model.player.maxSpeed - 4;
+            model.player.jumpTakeOffSpeed = model.player.jumpTakeOffSpeed + 1;
+        }
+    }
+}
---
+++ MyFirstGame/Assets/Scripts/Gameplay/Items/RareGreenMushInstance.cs
@@ -0,0 +1,62 @@
+﻿using Platformer.Gameplay;
+using UnityEngine;
+using Platformer.Model;
+using static Platformer.Core.Simulation;
+using static Platformer.Mechanics.Health;
+using static Platformer.Mechanics.HealthManager;
+
+
+namespace Platformer.Mechanics
+{
+    [RequireComponent(typeof(Collider2D))]
+    public class RareGreenMushInstance : MonoBehaviour
+    {
+        readonly PlatformerModel model = Core.Simulation.GetModel<PlatformerModel>();
+        public AudioClip greenMushCollectAudio;
+        [Tooltip("If true, animation will start at a random position in the sequence.")]
+        public bool randomAnimationStartTime = false;
+        [Tooltip("List of frames that make up the animation.")]
+        public Sprite[] idleAnimation, collectedAnimation;
+
+        internal Sprite[] sprites = new Sprite[0];
+
+        internal SpriteRenderer _renderer;
+
+        internal int greenMushIndex = -1;
+        internal GreenMushController controller;
+        internal int frame = 0;
+        internal bool collected = false;
+
+        void Awake()
+        {
+            _renderer = GetComponent<SpriteRenderer>();
+            if (randomAnimationStartTime)
+                frame = Random.Range(0, sprites.Length);
+            sprites = idleAnimation;
+        }
+
+        public void AddAbility()
+        {
+            maxHP = maxHP + 2;
+            currentHP = currentHP + 2;
+            model.player.maxSpeed = model.player.maxSpeed - 1;
+            HealthManager.instance.GetHealth();
+        }
+
+        public void RemoveAbility()
+        {
+            if (currentHP > 2)
+            {
+                maxHP = maxHP - 2;
+                currentHP = currentHP - 2;
+            }
+            else
+            {
+                maxHP = 1;
+                currentHP = 1;
+            }
+            model.player.maxSpeed = model.player.maxSpeed + 1;
+            HealthManager.instance.GetHealth();
+        }
+    }
+}
